921acb95fd94bda60bb88e79a95a393813c5eb58
Christian Imhorst <christian.imhorst@gmail.com>
2020-11-18 15:23:54 +0100

    Add files via upload

---
+++ UserNameInfoClass/UserNameInfo.vb
@@ -0,0 +1,28 @@
+﻿Public Class UserNameInfo
+    Private userNameValue As String
+
+    Public Sub New(UserName As String)
+        Me.UserName = UserName
+    End Sub
+
+    Public Property UserName() As String
+        Get
+            Return userNameValue
+        End Get
+        Set(ByVal Value As String)
+            userNameValue = Value
+        End Set
+    End Property
+
+    Public Sub Reverse()
+        ' Den Username rückwärts ausgeben
+        Dim charArray As Char() = userNameValue.ToCharArray()
+        Array.Reverse(charArray)
+        userNameValue = charArray
+        ' Alle Buchstaben klein schreiben
+        userNameValue = userNameValue.ToLower
+        ' Den ersten wieder groß schreiben, wenn alles rückwärts ist
+        userNameValue = userNameValue(0).ToString.ToUpper & userNameValue.Substring(1)
+    End Sub
+
+End Class
